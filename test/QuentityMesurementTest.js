fc33e191079b64ef9d77dd3b1df4705e04361d7c
shitalSonwane <shitals177@gmail.com>
2020-04-21 16:52:34 +0530

    Added:Test Cases For INCH

--- test/QuentityMesurementTest.js
+++ test/QuentityMesurementTest.js
@@ -32,4 +32,34 @@ describe("Tesing for length",function(){
         let ans=lenghth1.compare(lenghth2)
         assert.equal(ans,false)
     })
+
+    it("should return equal when 1 inch and 1 inch",function(){
+        let lenghth1=new quantityMeasurement(unit.INCH,0)
+        let lenghth2=new quantityMeasurement(unit.INCH,0)
+        let ans=lenghth1.compare(lenghth2)
+        assert.equal(ans,true)
+    })
+    it("should return not equal when 2 inch and 1 inch",function(){
+        let lenghth1=new quantityMeasurement(unit.INCH,0)
+        let lenghth2=new quantityMeasurement(unit.INCH,1)
+        let ans=lenghth1.compare(lenghth2)
+        assert.equal(ans,false)
+    })
+    it("should return not equal when null inch and 1 inch",function(){
+        let lenghth1=new quantityMeasurement(unit.INCH,null)
+        let lenghth2=new quantityMeasurement(unit.INCH,12)
+        let ans=lenghth1.compare(lenghth2)
+        assert.equal(ans,false)
+    })
+    it("should return equal when same refrance object of inch",function(){
+        let lenghth1=new quantityMeasurement(unit.INCH,1.0)
+        let ans=lenghth1.compare(lenghth1)
+        assert.equal(ans,true)
+    })
+    it("should return not equal when type different unit",function(){
+        let lenghth1=new quantityMeasurement(unit.FEET,1)
+        let lenghth2=new quantityMeasurement(unit.INCH,2)
+        let ans=lenghth1.compare(lenghth2)
+        assert.equal(ans,false)
+    })
 })
